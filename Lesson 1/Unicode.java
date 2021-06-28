042e27ed2ec29eeb64eeddedb6c24556b71b11ea
YasherStrasti <68648557+YasherStrasti@users.noreply.github.com>
2021-06-28 19:30:35 +0300

    Delete Lesson 1 directory

--- Lesson 1/Unicode.java
+++
@@ -1,9 +0,0 @@
-public class Unicode {
-    public static void main(String[] args) {
-        System.out.println("Вывод символов:");
-        
-        for(char ch = 33; ch <= 126; ch++) {
-            System.out.print(ch + " ");
-        }
-    }
-}
