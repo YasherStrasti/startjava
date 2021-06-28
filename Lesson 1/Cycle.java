71a0f53d17af62dc790dfa7c80a5e4e63092bbf3
YasherStrasti <68648557+YasherStrasti@users.noreply.github.com>
2021-06-28 15:45:43 +0300

    Add files via upload

---
+++ Lesson 1/Cycle.java
@@ -0,0 +1,31 @@
+public class task1 {
+    public static void main(String[] args) {
+        int numWhile = 6;
+        int numDoWhile = 10;
+        int sum = 0;
+        System.out.println("Цикл for:");
+
+        for(int i = 0; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Цикл while:");
+
+        while(numWhile >= -6) {
+            System.out.print(numWhile + " ");
+            numWhile = numWhile - 2;
+        }
+
+        System.out.println();
+        System.out.println("Цикл do-while: ");
+
+        do {
+            if(numDoWhile % 2 != 0) {
+                sum = sum + numDoWhile;
+            }
+            numDoWhile++;
+        } while(numDoWhile <= 20);
+        
+        System.out.println(sum + " "); //Сумма нечетных чисел
+    }
+}
