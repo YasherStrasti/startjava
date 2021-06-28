9271184e7421cb423e3e57f0433749786370ffef
YasherStrasti <68648557+YasherStrasti@users.noreply.github.com>
2021-06-28 14:44:36 +0300

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,34 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int age = 30;
+        double height = 1.93;
+        char firstWordFirstname = 'M';
+        boolean male = true;
+
+        if(age > 20) {
+            System.out.println("Возраст:" + age);
+        }
+
+        if(male) {
+            System.out.println("Пол: мужской");
+        }
+
+        if(!male) {
+            System.out.println("Пол: женский");
+        }
+
+        if(height < 1.80) {
+            System.out.println("Рост меньше 1.80");
+        } else {
+            System.out.println("Рост больше 1.80");
+        }
+
+        if(firstWordFirstname == 'M') {
+            System.out.println("Первая буква имени: " + firstWordFirstname);
+        } else if(firstWordFirstname == 'I') {
+            System.out.println("Первая буква имени: " + firstWordFirstname);
+        } else {
+            System.out.println("Первая буква имени: " + firstWordFirstname);
+        }
+    }
+}
